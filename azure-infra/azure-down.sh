b243daa2895f8719011a0703f83ac4ab3182332b
Drew Rogers <drogers@chariotsolutions.com>
2023-11-06 14:55:16 -0500

    Azure Build

--- azure-infra/azure-down.sh
+++ azure-infra/azure-down.sh
@@ -5,4 +5,10 @@ source .env
 echo "------------"
 echo "We are about to run terraform destroy. Make sure you are running this script in the gitops-playground/azure-infra directory."
 sleep 10
-terraform destroy
+
+# Grab Azure env variables
+if [[  ! -z "${IS_GITHUB_ACTIONS}" ]]; then
+  terraform destroy -auto-approve
+else
+  terraform destroy
+fi
