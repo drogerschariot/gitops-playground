fc67ac0e2038605f41617e13127ea5e260e7dd24
Drew Rogers <drogers@chariotsolutions.com>
2023-11-07 15:18:55 -0500

    Update

--- aws-infra/aws-down.sh
+++ aws-infra/aws-down.sh
@@ -23,6 +23,7 @@ echo "------------"
 echo "We are about to run terraform destroy. Make sure you are running this script in the gitops-playground/aws-infra directory."
 sleep 10
 
+terraform init
 # Grab Azure env variables
 if [[  ! -z "${IS_GITHUB_ACTIONS}" ]]; then
   terraform destroy -auto-approve
--- azure-infra/azure-down.sh
+++ azure-infra/azure-down.sh
@@ -11,6 +11,7 @@ echo "------------"
 echo "We are about to run terraform destroy. Make sure you are running this script in the gitops-playground/azure-infra directory."
 sleep 10
 
+terraform init
 # Grab Azure env variables
 if [[  ! -z "${IS_GITHUB_ACTIONS}" ]]; then
   terraform destroy -auto-approve
