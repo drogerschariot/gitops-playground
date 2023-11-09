fdeedc66e93dda24eb6a70c05eb2bcc8a354a025
Drew Rogers <drogers@chariotsolutions.com>
2023-11-09 15:45:38 -0500

    update

--- aws-infra/aws-down.sh
+++ aws-infra/aws-down.sh
@@ -11,11 +11,12 @@ fi
 echo "Destroying Load Balancer..."
 #kubectl delete -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl delete -f ../argocd/argocd/nginx-ingress.yml --namespace argocd
-kubectl delete namespace ingress
+kubectl delete svc/ingress-nginx-controller -n ingress
+# kubectl delete namespace ingress
 
 # Remove metrics port to EKS ingress security group
 EKS_SG=`aws eks describe-cluster --name $TF_VAR_name-eks --query 'cluster.resourcesVpcConfig.clusterSecurityGroupId' --output text`
-NODE_SG=`aws ec2 describe-instances --filter "Name=tag:eks:cluster-name,Values=$TF_VAR_name-eks" --query Reservations[*].Instances[*].NetworkInterfaces[0].Groups[0].GroupId --output text | tail -1 | cut -d "   " -f 1`
+NODE_SG=`aws ec2 describe-instances --filter "Name=tag:eks:cluster-name,Values=$TF_VAR_name-eks" --query Reservations[*].Instances[*].NetworkInterfaces[0].Groups[0].GroupId --output text | head -n1 | awk '{print $1;}'`
 aws ec2 revoke-security-group-ingress --group-id $NODE_SG --protocol tcp --port 6443 --source-group $EKS_SG
 aws ec2 revoke-security-group-ingress --group-id $NODE_SG --protocol tcp --port 4443 --source-group $EKS_SG
 
