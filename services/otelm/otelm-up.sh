d31781bf64a1353614a5c1de203513c035fcae15
Drew Rogers <drogers@chariotsolutions.com>
2023-11-12 19:16:26 -0500

    Service/otelm (#14)
    * Update CNPG readme
    
    * Update README.md
    
    * Add Otelm
    
    * Add Otelm

---
+++ services/otelm/otelm-up.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+#Check if base install script was run and ArgoCD exists
+if [ "`kubectl get pods -l app.kubernetes.io/name=argocd-server -o jsonpath='{.items[*].status.containerStatuses[0].ready}' --namespace argocd`" != "true" ]; then
+  echo "It seems the base install script was not run. Please follow https://github.com/drogerschariot/gitops-playground#install"
+  exit 1
+fi
+
+echo "Installing Otelm"
+kubectl apply -f otelm.yml
+sleep 30
+kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=opentelemetry-operator -n otelm-operator
+sleep 30
+
